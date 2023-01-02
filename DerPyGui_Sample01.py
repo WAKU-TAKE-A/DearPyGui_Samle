ad07e37e7f873e2ce44a4a744f4417d56a5c1f25
WAKU-TAKE-A <takehito.nishida@gmail.com>
2023-01-02 21:01:23 +0900

    Create DerPyGui_Sample01.py

---
+++ DerPyGui_Sample01.py
@@ -0,0 +1,17 @@
+# サンプルスクリプト1
+
+import dearpygui.dearpygui as dpg
+
+dpg.create_context()
+dpg.create_viewport(title='Custom Title', width=600, height=300)
+
+with dpg.window(label="Example Window", pos=(10, 10), width=200, height=200):
+    dpg.add_text("Hello, world")
+    dpg.add_button(label="Save")
+    dpg.add_input_text(label="string", default_value="Quick brown fox")
+    dpg.add_slider_float(label="float", default_value=0.273, max_value=1)
+
+dpg.setup_dearpygui()
+dpg.show_viewport()
+dpg.start_dearpygui()
+dpg.destroy_context()
