4e9fa44470776ea7a450ae1e023789b725d92b5b
WAKU-TAKE-A <takehito.nishida@gmail.com>
2023-01-02 21:04:55 +0900

    Create DearPyGui_Sample02.py

---
+++ DearPyGui_Sample02.py
@@ -0,0 +1,16 @@
+# サンプルスクリプト2
+
+import dearpygui.dearpygui as dpg
+
+dpg.create_context()
+dpg.create_viewport(title='Custom Title', width=600, height=300)
+
+with dpg.window(label="Label of Window1", pos=(10, 10), width=250, height=200) as window1:
+    button1 = dpg.add_button(label="My parerent is window1.")
+
+button2 = dpg.add_button(label="My parerent is Window1, too.", parent=window1)
+
+dpg.setup_dearpygui()
+dpg.show_viewport()
+dpg.start_dearpygui()
+dpg.destroy_context()
